04355ee5d17337b87c528308f8475f9b402f5c23
Hyunji Cho <hcho@myseneca.ca>
2020-07-03 04:35:58 +0900

    before heatmap

--- server.js
+++ server.js
@@ -1,11 +1,59 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+///////////////////////////////////////////////////
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
+    console.log("Express http server listening on " + HTTP_PORT);
+}
+
+///////////////////////////////////////////////////
+
+app.use(express.static("./public/"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+///////////////////////////////////////////////////
+
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+        navLink: function (url, options) {
+            return '<li' +
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+                '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        }
+    }
+}));
+
+app.set('view engine', '.hbs');
+
+///////////////////////////////////////////////////
+
+app.use(function (req, res, next) {
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
 });
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+///////////////////////////////////////////////////
+
+app.get("/", function (req, res) {
+    res.render('home');
+});
+
+app.get("/heatMap", function (req, res) {
+    res.render('heatMap');
+});
+
+///////////////////////////////////////////////////
+
+app.use(function (req, res) {
+    res.status(404).render('error');
+});
+
+app.listen(HTTP_PORT, onHttpStart);
